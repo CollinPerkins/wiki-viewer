073cdda2546b70b2bf5a3519b61ad0515a47a2d7
Collin Perkins <collinperkinsweb@gmail.com>
2016-09-13 17:18:05 -0500

    added styling

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 $( document ).ready(function() {
   $('#search').click(function () {
-    $('.contain').addClass('hidden');
     var query = $('#query').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ query +"&format=json&callback=?";
 
-    $.ajax({
-      url: url,
-      type: "GET",
-      dataType: "json",
-      success: function(results){
-        console.log(results)
-        for (var i = 0; i < results[1].length; i++) {
-          $('#wikiList').append('<li><div><h1><a href="' + results[3][i] + '">' + results[1][i] + '</a></h1><p>' + results[2][i] + '</p></div></li>')
-        }
-      }
-    });
-  });
+    if(query !== ""){
+      $('.contain').addClass('hidden');
+      $('#reSearch').removeClass('hidden');
+      var wikiData = $.getJSON(url);
 
+      wikiData.done(function(data) {
+        for (var i = 0; i < data[1].length; i++) {
+          $('#wikiList').append('<li><div><h1><a href="' + data[3][i] + '">' + data[1][i] + '</a></h1><p>' + data[2][i] + '</p></div></li>');
+        }
+      });
+
+      wikiData.fail(function() {
+        $('#wikiList').append('<p>Oh no, something went wrong!</p>');
+      });
+    }
+  });
 });
