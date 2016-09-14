c7d7243a2bc2f4f4ef3964f97fdd80b4b2b28f92
Collin Perkins <collinperkinsweb@gmail.com>
2016-09-14 09:17:07 -0500

    finished Wiki

--- main.js
+++ main.js
@@ -19,4 +19,8 @@ $( document ).ready(function() {
       });
     }
   });
+  $('#reSearch').click(function () {
+    $('.contain').removeClass('hidden');
+    $('#reSearch').addClass('hidden');
+  });
 });
